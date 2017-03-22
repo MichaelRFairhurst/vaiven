2a6faca7a5cb848230ba2be057e900c8f21cc2c4
Mike Fairhurst <mfairhurst@google.com>
2017-03-22 10:49:42 -0700

    Fix gc test

--- bench/loop_with_garbagemem_cc.vvn.sh
+++ bench/loop_with_garbagemem_cc.vvn.sh
@@ -3,13 +3,13 @@
 cat <<EOF
 fn loop
   var x = 1
-  var a = 0
+  var a = []
   for x < 50000000
     x = x + 1
     if x / 50000 == 0
-      a = append("foo", "bar")
+      a = "foo" + "bar"
     else
-      a = listWith(a)
+      a = []
     end
   end
 end
